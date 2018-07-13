862230d37e7eddd3115b6e04d7a6996b86ab5a21
Pragyachhabra <pragyachhabbra@gmail.com>
2018-07-13 20:09:42 +0200

    [new] javascript?video

---
+++ app.js
@@ -0,0 +1,14 @@
+// Adtoox Javascript Test
+
+// feel free to add more functions and/or files
+// to structure your code as needed.
+
+/**
+ * @param {Object} resources - slideshow resources.
+ * @param {{mp4:string, webm:string}} resources.video - video resource, webm and mp4 urls provided.
+ * @param {string[]} resources.images - image resources, png urls
+ * @param {string} resources.targetUrl - target url for slideshow click
+ */
+function startApp(resources) {
+}
+
